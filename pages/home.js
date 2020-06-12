a36e1c1af6c70b9acb44e3702b4e2de39c505127
overactiveimagination <34569355+overactiveimagination@users.noreply.github.com>
2020-06-12 09:28:02 +0200

    Initial commit

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+import Nav from '../components/nav'
+
+export default () => (
+  <div>
+    <Nav />
+    <div className="hero">
+      <h1 className="title">Next.js + Tailwind CSS</h1>
+    </div>
+  </div>
+  <div>
+    <Nav />
+    <div className="hero">
+      <h1 className="title">Next.js + Tailwind CSS</h1>
+  </div>
+  </div>
+
+)
