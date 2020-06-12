db0eb288c3d067a08da918b927287068157d6d45
overactiveimagination <34569355+overactiveimagination@users.noreply.github.com>
2020-06-12 09:32:37 +0200

    Fix home

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,18 @@
-import Nav from '../components/nav'
+import Nav from "../components/nav";
 
 export default () => (
-  <div>
-    <Nav />
-    <div className="hero">
-      <h1 className="title">Next.js + Tailwind CSS</h1>
+  <>
+    <div>
+      <Nav />
+      <div className="hero">
+        <h1 className="title">Next.js + Tailwind CSS</h1>
+      </div>
     </div>
-  </div>
-  <div>
-    <Nav />
-    <div className="hero">
-      <h1 className="title">Next.js + Tailwind CSS</h1>
-  </div>
-  </div>
-
-)
+    <div>
+      <Nav />
+      <div className="hero">
+        <h1 className="title">Next.js + Tailwind CSS</h1>
+      </div>
+    </div>
+  </>
+);
